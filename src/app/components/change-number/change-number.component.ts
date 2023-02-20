97dfc0db9b4a430821a61ab73a7d22fc4733a456
digaogalo <digaolimac@gmail.com>
2023-02-20 15:17:04 -0300

    emit event

---
+++ src/app/components/change-number/change-number.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-number',
+  templateUrl: './change-number.component.html',
+  styleUrls: ['./change-number.component.css']
+})
+export class ChangeNumberComponent {
+  handleClick() {
+    console.log('Mudou o número')
+  }
+
+}
---
+++ src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent {
+  show: boolean = false
+
+  showMessage(): void {
+    this.show = !this.show //toggle
+
+  }
+
+}
