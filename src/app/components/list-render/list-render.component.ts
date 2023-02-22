654a11018652645413860536476813e04f321074
digaogalo <digaolimac@gmail.com>
2023-02-22 18:01:46 -0300

    router angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
+
+import { ListService } from 'src/app/services/list.service';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
+  constructor(private listService: ListService) {}
   animals: Animal[] = [
       {name: 'Turca', type:  'Dog', age: 3},
       {name: 'Cristal', type:  'Cat', age: 5},
@@ -18,5 +21,10 @@ export class ListRenderComponent {
 
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos.`
+  }
+
+  removeAnimal(animal: Animal) {
+    console.log('Removendo animal ...')
+    this.animals =  this.listService.remove(this.animals, animal)
   }
 }
